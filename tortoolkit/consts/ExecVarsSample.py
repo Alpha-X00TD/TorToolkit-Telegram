358b0d716ce411280abae45d37f34277920f5bbc
Alpha-X00TD <78870182+Alpha-X00TD@users.noreply.github.com>
2021-05-17 10:18:04 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -5,16 +5,16 @@ except:
         # Set true if its VPS
         IS_VPS = False
         
-        API_HASH = ""
-        API_ID = 0
-        BOT_TOKEN = ""
-        BASE_URL_OF_BOT = ""
+        API_HASH = "42e520063120b19142822fcb7a043b4d"
+        API_ID = 3409279
+        BOT_TOKEN = "1782070147:AAGSlogAIij91lM0RtEzOrQ_sbUGlucxa5U"
+        BASE_URL_OF_BOT = "http://t.me/Leech21bot"
 
         # Edit the server port if you want to keep it default though.
         SERVPORT = 80
 
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = []
+        ALD_USR = [1557573671,1001328551758]
         OWNER_ID = 0
         
         # Google Drive Index Link should include the base dir also See readme for more info
@@ -39,7 +39,7 @@ except:
         DB_URI = "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
         
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
-        #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
+        #DB_URI = "postgresql://postgres:dkkaj0123456@postgresql/postgres"
         
         # The base direcory to which the files will be upload if using RCLONE
         RCLONE_BASE_DIR = "/"
@@ -50,13 +50,13 @@ except:
 
         # Will be enabled once its set
         # For vps change it to True if config loaded
-        RCLONE_ENABLED = False
+        RCLONE_ENABLED = True
 
         # If the user fails to select whether to use rclone or telegram to upload this will be the deafult.
         DEFAULT_TIMEOUT = "leech"
 
         # For vps set path here or you can use runtime too
-        RCLONE_CONFIG = False
+        RCLONE_CONFIG = True
         
         # Name of the RCLONE drive from the config
         DEF_RCLONE_DRIVE = ""
